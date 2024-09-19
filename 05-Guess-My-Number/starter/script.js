f1a221e1e3c5931786af96bf0ce2cf19a45f4255
IsaacToluwani <toluwani.isaac@gmail.com>
2024-09-19 21:07:06 +0100

    practice continues

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,21 +1,50 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number';
+// // Generate Random Number
+// const ranNumber = Math.trunc(Math.random() * 21);
+// document.querySelector('.number').textContent = ranNumber;
+// console.log(ranNumber, typeof ranNumber);
 
-// document.querySelector('.guess').value = '26';
+// // React to Click
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
 
-// document.querySelector('.score').textContent = 10;
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'No Number';
+//   } else if (guess === ranNumber) {
+//     document.querySelector('.message').textContent = 'Correct Number';
+//   } else if (guess < ranNumber) {
+//     document.querySelector('.message').textContent = 'Too Low';
+//   } else if (guess > ranNumber) {
+//     document.querySelector('.message').textContent = 'Too High';
+//   }
+// });
 
 // Generate Random Number
-const ranNumber = Math.trunc(Math.random() * 21);
-document.querySelector('.number').textContent = ranNumber;
-console.log(ranNumber, typeof ranNumber);
 
-// React to Click
+const ranNumber = Math.floor(Math.random() * 20 + 1);
+console.log(typeof ranNumber, ranNumber);
+
+// Take input from User
+
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+  let userInput = Number (document.querySelector('.guess').value);
+  console.log(typeof userInput, userInput);
+  
+  if (!userInput) {
+    document.querySelector('.message').textContent = 'Wrong Number';
+  } else if (userInput === ranNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (userInput > ranNumber) {
+    document.querySelector('.message').textContent = 'Too High';
+    let cScore = (Number (document.querySelector('.score').textContent)) - 1;
+    document.querySelector('.score').textContent = cScore;
+  } else if (userInput < ranNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
+    let cScore = (Number (document.querySelector('.score').textContent)) - 1;
+    document.querySelector('.score').textContent = cScore;
   }
 });
+
+let cScore = (document.querySelector('score').value) - 1;
